ddd20482bbff77957759fe7490bddf4baacf5505
Juan Carlos Ojeda G <juanki.ijcog@gmail.com>
2018-04-23 07:02:39 -0300

    refactoring

--- analisador.go
+++ analisador.go
@@ -4,93 +4,91 @@ import (
 	"strings"
 )
 
-const cantSequence int = 4
+const cantSequenceRepetition int = 4
+const cantSequenceMin int = 2
 
-var letrasCodigoGenetico = []string{"A", "T", "C", "G"}
+var geneticCodeLetters = []string{"A", "T", "C", "G"}
 var secuenceFound int = 0
+var sizeMatriz int = 0
 
 func isMutant(adn []string) bool {
-	if buscarEnFilas(adn) {
-		return true
-	}
-	if buscarEnColumnas(adn) {
-		return true
-	}
-	if buscarEnDiagonales(adn) {
+	sizeMatriz = sizeMatriz
+	if findInRows(adn) || findInColumns(adn) || findInDiagonals(adn) {
 		return true
 	}
 	return false
 }
 
-func validar() bool {
-	return cantSequence <= secuenceFound
+func valid() bool {
+	return cantSequenceMin <= secuenceFound
 }
 
-func buscarEnFilas(adn []string) bool {
-	for index := 0; index < len(adn); index++ {
-		if buscarEnFila(adn[index]) {
-			secuenceFound++
-		}
-		if validar() {
-			return true
+func findInRows(adn []string) bool {
+	for index := 0; index < sizeMatriz; index++ {
+		if findInRow(adn[index]) {
+			if addFound() {
+				return true
+			}
 		}
 	}
 	return false
 }
 
-func buscarEnColumnas(adn []string) bool {
-	for i := 0; i < len(adn); i++ {
+func findInColumns(adn []string) bool {
+	for i := 0; i < sizeMatriz; i++ {
 		column := ""
-		for j := 0; j < len(adn); j++ {
+		for j := 0; j < sizeMatriz; j++ {
 			column += string(adn[j][i])
 		}
-		if buscarEnFila(column) {
-			secuenceFound++
-		}
-		if validar() {
-			return true
+		if findInRow(column) {
+			if addFound() {
+				return true
+			}
 		}
 	}
 	return true
 }
 
-func buscarEnDiagonales(adn []string) bool {
-	n := len(adn)
-	for i := 0; i < n; i++ {
+func findInDiagonals(adn []string) bool {
+	for i := 0; i < sizeMatriz; i++ {
 		var diagonals [4]string
 		for j := 0; j <= i; j++ {
-			x := (n - 1) - j
-			y := (n - 1) - (i - j)
+			x := (sizeMatriz - 1) - j
+			y := (sizeMatriz - 1) - (i - j)
 			diagonals[0] += string(adn[j][i-j])
 			diagonals[1] += string(adn[x][y])
 			diagonals[2] += string(adn[j][y])
 			diagonals[3] += string(adn[y][j])
 		}
-		if revisarDiagonales(diagonals) {
+		if checkInDiagonals(diagonals) {
 			return true
 		}
 	}
 	return false
 }
 
-func buscarEnFila(fila string) bool {
-	for i := 0; i < len(letrasCodigoGenetico); i++ {
-		fined := strings.Repeat(letrasCodigoGenetico[i], cantSequence)
-		if strings.Contains(fila, fined) {
+func findInRow(row string) bool {
+	for i := 0; i < len(geneticCodeLetters); i++ {
+		fined := strings.Repeat(geneticCodeLetters[i], cantSequenceRepetition)
+		if strings.Contains(row, fined) {
 			return true
 		}
 	}
 	return false
 }
 
-func revisarDiagonales(diagonals [4]string) bool {
+func checkInDiagonals(diagonals [4]string) bool {
 	for i := 0; i < len(diagonals); i++ {
-		if buscarEnFila(diagonals[i]) {
-			secuenceFound++
+		if findInRow(diagonals[i]) {
+			if addFound() {
+				return true
+			}
 		}
 	}
-	if validar() {
-		return true
-	}
 	return false
+}
+
+func addFound() bool {
+	secuenceFound++
+	return valid()
 }
