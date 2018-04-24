6c2961f2e8d82fd7dab24bfe8e48c020e18f6fa5
Juan Carlos Ojeda G <juanki.ijcog@gmail.com>
2018-04-24 18:23:18 -0300

    test unit modification for more cases

--- mutant_test.go
+++ mutant_test.go
@@ -2,23 +2,35 @@ package main
 
 import "testing"
 
+type Case struct {
+	Matriz   []string
+	Response bool
+}
+
 func TestIsMutant(t *testing.T) {
-  adn := [][]string{{"ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"},{
-            "ABCDABCDA",
-            "EAGHEFGHB",
-            "IJUMIJKMC",
-            "LNIALNYOD",
-            "ABCDXBCDE",
-            "EFGHEAGHF",
-            "IJKMIJAMG",
-            "LNSOLNTAH",
-            "LNUOLNXOA"},
-            {"ABCD","AAGH","AJAM","ANNA"}
-          }
-  for i := 0; i < leng(adn); i++ {
-    isMutant := isMutant(adn[i])
-    if !isMutant {
-      t.Errorf("isMutant was incorrect, got: %d, want: %d.", isMutant, true)
-    }
-  }
+	var cases = []Case{
+		Case{
+			Matriz:   []string{"AALABCDAX", "ECGCEFGAB", "IJCMCJXMC", "LNIYLVYOD", "ABCDCBNDE", "EFGHECGHF", "IJKMIJCMG", "LNSOLNTCH", "AAXALNXOC"},
+			Response: false,
+		},
+		Case{
+			Matriz:   []string{"ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"},
+			Response: true,
+		},
+		Case{
+			Matriz:   []string{"AALABCDAT", "ECGCEFGTB", "IJCMCJVMC", "LNICLTYOD", "ABCDVBNDE", "EFGTECGHF", "IJTMIJCMG", "LTSOLNTTH", "TAXALNXOC"},
+			Response: false,
+		},
+		Case{
+			Matriz:   []string{"AALABCDAT", "ECGCEFGTB", "IJCMCJTMC", "LNICLTYOD", "ABCDVBNDE", "EFGTECGHF", "IJTMIJCMG", "LTSOLNTTH", "TAXALNXOC"},
+			Response: true,
+		},
+	}
+
+	for i := 0; i < len(cases); i++ {
+		isMutant := isMutant(cases[i].Matriz)
+		if isMutant != cases[i].Response {
+			t.Errorf("isMutant was incorrect, got: %d, want: %d. Case: %d", isMutant, cases[i].Response, i+1)
+		}
+	}
 }
